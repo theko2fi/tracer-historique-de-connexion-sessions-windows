ffa77fdf3ca11056fc2d111cbac05fe54e54ce5b
theko2fi <72862222+theko2fi@users.noreply.github.com>
2022-07-13 23:29:01 +0100

    Add files via upload

---
+++ tracer_historique_de_connexion_des_sessions_windows/TRACK_STARTUP.ps1
@@ -0,0 +1,11 @@
+﻿$bj="STARTUP"
+$date=[datetime]::Now
+$datetimeString=[String]$date
+$date_time=$datetimeString.replace("/","-").Split(" ")
+
+$LogonUser=$env:USERNAME
+$destination=$date_time[0]+"_"+$env:COMPUTERNAME
+
+$res=$date_time[0]+","+$date_time[1]+","+$env:COMPUTERNAME+","+$LogonUser+","+$bj
+
+Add-Content \\VOLAZI-SRV-G3\G3_SESSION_RECORD$\$destination.txt "$res"
